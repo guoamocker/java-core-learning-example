d67b692311c6f21e64a0b15a6193bb41cfd31323
guoxin <guoxin@newrank.cn>
2020-01-02 11:25:42 +0800

    check  reflection

---
+++ src/main/java/org/javacore/reflection/bean/User.java
@@ -0,0 +1,18 @@
+package org.javacore.reflection.bean;
+
+import lombok.Data;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author gx
+ * @Date 2020/1/2 11:24
+ */
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+}
